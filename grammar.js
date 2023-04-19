3c8ea24eaeca285a8f6093ccd1ad8baf99890f31
Colin Kennedy <colinvfx@gmail.com>
2023-04-18 21:01:23 -0700

    Got the base example working

--- grammar.js
+++ grammar.js
@@ -2,8 +2,14 @@ module.exports = grammar(
     {
         name: "usd",
 	rules: {
-	    // TODO: Add the actual grammar rules
-	    shebang: $ => "#usda 1.0",  // TODO: make value, variable
+	    prim_definition: $ => seq(
+		"def",
+		field("name", seq('"', $.identifier, '"')),  // TODO: Add string literal, here
+		$.block,
+	    ),
+
+	    // // TODO: Add the actual grammar rules
+	    // shebang: $ => "#usda 1.0",  // TODO: make value, variable
 
 	    _class: $ => "class",
 	    _def: $ => "def",
@@ -24,27 +30,20 @@ module.exports = grammar(
 	    // TODO: Make sure this works. Can prims be named with )@(*#$& special characters?
 	    _schema_type: $ => /[a-z0-9]+/i,
 
+	    // TODO: Finish
+	    _statement: $ => "",
+
 	    // TODO: Double-check what is allowed, as an identifier
 	    identifier: $ => /[\p{L}_$][\p{L}\p{Nd}_$]*/,
 
 	    string_literal: $ => choice($._string_literal, $._multiline_string_literal),
 
-	    prim_definition: $ => seq(
-		field("prim_type", choice($._class, $._def, $._over)),
-		optional(field("schema_type", $._schema_type)),
-		field("name", $.identifier),
-	    ),
-
 	    block: $ => seq(
-		'{',
+		"{",
 		// TODO: Add attribute / variant / etc implementation
-		// repeat($._statement),
-		'}'
+		// optional(repeat($._statement)),
+		"}"
 	    )
-
-	    // TODO: Finish
-	    // _statement: $ => choice(
-	    // )
 	}
     }
 )
