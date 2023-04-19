f9500d2a2d047875a918b348e05347ef0bfcc5e3
Colin Kennedy <colinvfx@gmail.com>
2023-04-18 23:27:59 -0700

    Added new grammar

--- grammar.js
+++ grammar.js
@@ -3,6 +3,7 @@ module.exports = grammar(
         name: "usd",
         rules:
         {
+            // TODO: Fix
             // sheline: $ => seq(
             //     "#",
             //     /[a-z]+ [\d+\.]+/
@@ -13,49 +14,96 @@ module.exports = grammar(
 
             prim_definition: $ => seq(
                 field("prim_type", choice("class", "def", "over")),
-                // optional(field("schema_type", "blah")),
-                // field("name", '"foo"'),  // TODO: Add string literal, here
-                // $.block,
+                optional(field("schema_type", $.identifier)),
+                field("name", seq('"', $.identifier, '"')),  // TODO: Add string literal, here
+                optional($.block),
             ),
 
-            // body: $ => optional(repeat($.prim_definition)),
-            //
-            // prim_definition: $ => seq(
-            //     field("prim_type", choice("class", "def", "over")),
-            //     // optional(field("schema_type", "blah")),
-            //     // field("name", '"foo"'),  // TODO: Add string literal, here
-            //     // $.block,
-            // ),
-            //
-            // block: $ => seq("{","}"),
+            attribute_definition: $ => seq(
+                $._attribute_declaration,
+                "=",
+                $._value_list,
+            ),
 
-            // prim_definition: $ => seq(
-            //     field("prim_type", choice("class", "def", "over")),
-            //     optional(field("schema_type", $.identifier)),
-            //     field("name", seq('"', $.identifier, '"')),  // TODO: Add string literal, here
-            //     $.block,
-            // ),
-            //
-            // block: $ => seq(
-            //     "{",
-            //     // TODO: All recursive Prims / attributes
-            //     optional(choice($._attribute_declaration, $.attribute_definition)),
-            //     // TODO: Add attribute / variant / etc implementation
-            //     // optional(repeat($._statement)),
-            //     "}"
-            // ),
-            //
-            // attribute_definition: $ => seq(
-            //     $._attribute_declaration,
-            //     "=",
-            //     $._value_list,
-            // ),
-            //
-            // _attribute_declaration: $ => seq(
-            //     $._pattern_list,
-            //     $.identifier,
-            // ),
-            //
+            _attribute_declaration: $ => seq(
+                $._pattern_list,
+                $.identifier,
+            ),
+
+            _pattern_list: $ => seq($._attribute_type, $.identifier),  // TODO: Check if identifier has to be ASCII. might be stricter than a regular identifier
+
+            _attribute_type: $ => choice(
+                $._scalar_type,
+                $._dimentioned_type,
+                $._dictionary_type,
+            ),
+
+            _scalar_type: $ => choice(
+                "asset", "asset[]",
+                "bool", "bool[]",
+                "double", "double[]",
+                "float", "float[]",
+                "half", "half[]",
+                "int", "int[]",
+                // TODO: Add later
+                // "int64", "int64[]",
+                "string", "string[]",
+                "timecode", "timecode[]",
+                "token", "token[]",
+                "uchar", "uchar[]",
+                "uint", "uint[]",
+                "uint64", "uint64[]",
+            ),
+
+            _dictionary_type: $ => "dictionary",
+
+            _dimentioned_type: $ => choice(
+                "double2", "double2[]",
+                "double3", "double3[]",
+                "double4", "double4[]",
+                "float2", "float2[]",
+                "float3", "float3[]",
+                "float4", "float4[]",
+                "half2", "half2[]",
+                "half3", "half3[]",
+                "half4", "half4[]",
+                "int2", "int2[]",
+                "int3", "int3[]",
+                "int4", "int4[]",
+                "int64", "int64[]",
+                "matrix2d", "matrix2d[]",
+                "matrix3d", "matrix3d[]",
+                "matrix4d", "matrix4d[]",
+                "quatd", "quatd[]",
+                "quatf", "quatf[]",
+                "quath", "quath[]",
+            ),
+
+            // TODO: Finish
+            _value_list: $ => "8",
+
+
+            block: $ => seq(
+                "{",
+                optional(
+                    repeat(
+                        choice(
+                            $.prim_definition,
+
+                            // $._attribute_declaration,  // TODO: Add later
+                            $.attribute_definition,
+                        )
+                    )
+                ),
+                // // TODO: All recursive Prims / attributes
+                // optional(choice($._attribute_declaration, $.attribute_definition)),
+                // // TODO: Add attribute / variant / etc implementation
+                // // optional(repeat($._statement)),
+                "}"
+            ),
+
+            identifier: $ => /[a-zA-Z0-9_]+/i,
+
             // identifier: $ => /[a-zA-Z0-9_]+/i,
             // string_literal: $ => choice($._string_literal, $._multiline_string_literal),
             //
