d86f7fb36bed99cfff6968b3f6441fb99ce1f97d
Colin Kennedy <colinvfx@gmail.com>
2023-06-03 21:34:22 -0700

    Added GPL3.0 license

--- grammar.js
+++ grammar.js
@@ -1,3 +1,20 @@
+// Copyright (C) THEDATE Colin Kennedy
+// This file is part of tree-sitter-usd <https://github.com/ColinKennedy/tree-sitter-usd>.
+//
+// tree-sitter-usd is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// tree-sitter-usd is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with tree-sitter-usd.  If not, see <http://www.gnu.org/licenses/>.
+
+
 module.exports = grammar(
     {
         name: "usd",
